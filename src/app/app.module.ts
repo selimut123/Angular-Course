cda0873ffb074aea87a69ba2d9eebb4173335bac
Bryan Christiano <bryantrack1@gmail.com>
2024-01-21 19:35:23 -0800

    Creating and Adding Server Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
